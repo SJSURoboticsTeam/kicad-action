6b9f22cbc47c20b298e5b9bfd8dd8747ddc90d3d
Shin Umeda <umeda.shin@gmail.com>
2025-09-02 18:05:45 -0700

    modify script to allow for multiple inputs, add test workflow for multiple inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,76 +8,93 @@ cp -r /home/kicad/.config/kicad $HOME/.config/
 erc_violation=0 # ERC exit code
 drc_violation=0 # DRC exit code
 
+IFS=';'
+
 # Run ERC if requested
 if [[ -n $INPUT_KICAD_SCH ]] && [[ $INPUT_SCH_ERC = "true" ]]
 then
+for file in $INPUT_KICAD_SCH
   kicad-cli sch erc \
-    --output "`dirname $INPUT_KICAD_SCH`/$INPUT_SCH_ERC_FILE" \
+    --output "$(dirname $file)/$INPUT_SCH_ERC_FILE" \
     --format $INPUT_REPORT_FORMAT \
     --exit-code-violations \
-    "$INPUT_KICAD_SCH"
+    "$file"
   erc_violation=$?
+end
 fi
 
 # Export schematic PDF if requested
 if [[ -n $INPUT_KICAD_SCH ]] && [[ $INPUT_SCH_PDF = "true" ]]
 then
+for file in $INPUT_KICAD_SCH
   kicad-cli sch export pdf \
-    --output "`dirname $INPUT_KICAD_SCH`/$INPUT_SCH_PDF_FILE" \
-    "$INPUT_KICAD_SCH"
+    --output "$(dirname $file)/$INPUT_SCH_PDF_FILE" \
+    "$file"
+end
 fi
 
 # Export schematic BOM if requested
 if [[ -n $INPUT_KICAD_SCH ]] && [[ $INPUT_SCH_BOM = "true" ]]
 then
+for file in $INPUT_KICAD_SCH
   kicad-cli sch export bom \
-    --output "`dirname $INPUT_KICAD_SCH`/$INPUT_SCH_BOM_FILE" \
+    --output "$(dirname $file)/$INPUT_SCH_BOM_FILE" \
     --preset "$INPUT_SCH_BOM_PRESET" \
-    "$INPUT_KICAD_SCH"
+    "$file"
+end
 fi
 
 # Run DRC if requested
 if [[ -n $INPUT_KICAD_PCB ]] && [[ $INPUT_PCB_DRC = "true" ]]
 then
+for file in $INPUT_KICAD_PCB
   kicad-cli pcb drc \
-    --output "`dirname $INPUT_KICAD_PCB`/$INPUT_PCB_DRC_FILE" \
+    --output "$(dirname $file)/$INPUT_PCB_DRC_FILE" \
     --format $INPUT_REPORT_FORMAT \
     --exit-code-violations \
-    "$INPUT_KICAD_PCB"
+    "$file"
   drc_violation=$?
+end
 fi
 
 # Export Gerbers if requested
 if [[ -n $INPUT_KICAD_PCB ]] && [[ $INPUT_PCB_GERBERS = "true" ]]
 then
-  GERBERS_DIR=`mktemp -d`
+for file in $INPUT_KICAD_PCB
+  GERBERS_DIR=$(mktemp -d)
   kicad-cli pcb export gerbers \
     --output "$GERBERS_DIR/" \
-    "$INPUT_KICAD_PCB"
+    "$file"
   kicad-cli pcb export drill \
     --output "$GERBERS_DIR/" \
-    "$INPUT_KICAD_PCB"
+    "$file"
   zip -j \
-    "`dirname $INPUT_KICAD_PCB`/$INPUT_PCB_GERBERS_FILE" \
+    "$(dirname $file)/$INPUT_PCB_GERBERS_FILE" \
     "$GERBERS_DIR"/*
+end
 fi
 
 if [[ -n $INPUT_KICAD_PCB ]] && [[ $INPUT_PCB_IMAGE = "true" ]]
 then
-  mkdir -p "`dirname $INPUT_KICAD_PCB`/$INPUT_PCB_IMAGE_PATH"
+for file in $INPUT_KICAD_PCB
+  directory=$(dirname $file)
+  mkdir -p "$directory/$INPUT_PCB_IMAGE_PATH"
   kicad-cli pcb render --side top \
-    --output "`dirname $INPUT_KICAD_PCB`/$INPUT_PCB_IMAGE_PATH/top.png" \
-    "$INPUT_KICAD_PCB"
+    --output "$directory/$INPUT_PCB_IMAGE_PATH/top.png" \
+    "$file"
   kicad-cli pcb render --side bottom \
-    --output "`dirname $INPUT_KICAD_PCB`/$INPUT_PCB_IMAGE_PATH/bottom.png" \
-    "$INPUT_KICAD_PCB"
+    --output "$directory/$INPUT_PCB_IMAGE_PATH/bottom.png" \
+    "$file"
+end
 fi
 
 if [[ -n $INPUT_KICAD_PCB ]] && [[ $INPUT_PCB_MODEL = "true" ]]
 then
+for file in $INPUT_KICAD_PCB
   kicad-cli pcb export step $INPUT_PCB_MODEL_FLAGS \
-    --output "`dirname $INPUT_KICAD_PCB`/$INPUT_PCB_MODEL_FILE" \
-    "$INPUT_KICAD_PCB"
+    --output "$(dirname $file)/$INPUT_PCB_MODEL_FILE" \
+    "$file"
+end
 fi
 
 # Return non-zero exit code for ERC or DRC violations
