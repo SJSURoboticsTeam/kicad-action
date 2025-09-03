e4c2a614f34154796e32f86143fef18f28584e49
Shin Umeda <umeda.shin@gmail.com>
2025-09-02 18:08:39 -0700

    fix syntax errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,54 +13,54 @@ IFS=';'
 # Run ERC if requested
 if [[ -n $INPUT_KICAD_SCH ]] && [[ $INPUT_SCH_ERC = "true" ]]
 then
-for file in $INPUT_KICAD_SCH
+for file in $INPUT_KICAD_SCH; do
   kicad-cli sch erc \
     --output "$(dirname $file)/$INPUT_SCH_ERC_FILE" \
     --format $INPUT_REPORT_FORMAT \
     --exit-code-violations \
     "$file"
   erc_violation=$?
-end
+done
 fi
 
 # Export schematic PDF if requested
 if [[ -n $INPUT_KICAD_SCH ]] && [[ $INPUT_SCH_PDF = "true" ]]
 then
-for file in $INPUT_KICAD_SCH
+for file in $INPUT_KICAD_SCH; do
   kicad-cli sch export pdf \
     --output "$(dirname $file)/$INPUT_SCH_PDF_FILE" \
     "$file"
-end
+done
 fi
 
 # Export schematic BOM if requested
 if [[ -n $INPUT_KICAD_SCH ]] && [[ $INPUT_SCH_BOM = "true" ]]
 then
-for file in $INPUT_KICAD_SCH
+for file in $INPUT_KICAD_SCH; do
   kicad-cli sch export bom \
     --output "$(dirname $file)/$INPUT_SCH_BOM_FILE" \
     --preset "$INPUT_SCH_BOM_PRESET" \
     "$file"
-end
+done
 fi
 
 # Run DRC if requested
 if [[ -n $INPUT_KICAD_PCB ]] && [[ $INPUT_PCB_DRC = "true" ]]
 then
-for file in $INPUT_KICAD_PCB
+for file in $INPUT_KICAD_PCB; do
   kicad-cli pcb drc \
     --output "$(dirname $file)/$INPUT_PCB_DRC_FILE" \
     --format $INPUT_REPORT_FORMAT \
     --exit-code-violations \
     "$file"
   drc_violation=$?
-end
+done
 fi
 
 # Export Gerbers if requested
 if [[ -n $INPUT_KICAD_PCB ]] && [[ $INPUT_PCB_GERBERS = "true" ]]
 then
-for file in $INPUT_KICAD_PCB
+for file in $INPUT_KICAD_PCB; do
   GERBERS_DIR=$(mktemp -d)
   kicad-cli pcb export gerbers \
     --output "$GERBERS_DIR/" \
@@ -71,12 +71,12 @@ for file in $INPUT_KICAD_PCB
   zip -j \
     "$(dirname $file)/$INPUT_PCB_GERBERS_FILE" \
     "$GERBERS_DIR"/*
-end
+done
 fi
 
 if [[ -n $INPUT_KICAD_PCB ]] && [[ $INPUT_PCB_IMAGE = "true" ]]
 then
-for file in $INPUT_KICAD_PCB
+for file in $INPUT_KICAD_PCB; do
   directory=$(dirname $file)
   mkdir -p "$directory/$INPUT_PCB_IMAGE_PATH"
   kicad-cli pcb render --side top \
@@ -85,16 +85,16 @@ for file in $INPUT_KICAD_PCB
   kicad-cli pcb render --side bottom \
     --output "$directory/$INPUT_PCB_IMAGE_PATH/bottom.png" \
     "$file"
-end
+done
 fi
 
 if [[ -n $INPUT_KICAD_PCB ]] && [[ $INPUT_PCB_MODEL = "true" ]]
 then
-for file in $INPUT_KICAD_PCB
+for file in $INPUT_KICAD_PCB; do
   kicad-cli pcb export step $INPUT_PCB_MODEL_FLAGS \
     --output "$(dirname $file)/$INPUT_PCB_MODEL_FILE" \
     "$file"
-end
+done
 fi
 
 # Return non-zero exit code for ERC or DRC violations
